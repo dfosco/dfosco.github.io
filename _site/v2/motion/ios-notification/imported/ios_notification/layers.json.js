620f450b21a3bd131d32f31864131f3560a37c52
Daniel Fosco <danielfosco@gmail.com>
2015-04-28 17:17:39 -0300

    [danielfos.co] Archive views are not working correctly

---
+++ _site/v2/motion/ios-notification/imported/ios_notification/layers.json.js
@@ -0,0 +1,504 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["ios_notification/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "id" : "99FD0903-8F08-4475-B2D5-3AF620D1668D",
+    "visible" : true,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "50AB0289-DDE0-471E-ADDD-DAD3B553F1C7",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "112B2F1B-963A-43E7-BE01-EA372F4012EE",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "AACDA92C-7757-4A05-A13B-0741609FBAF4",
+                "visible" : true,
+                "children" : [
+                  {
+                    "maskFrame" : null,
+                    "id" : "6B2B3E94-2AC0-441F-9BAD-1C9C2C35E306",
+                    "visible" : true,
+                    "children" : [
+
+                    ],
+                    "image" : {
+                      "path" : "images\/Tweet-6B2B3E94-2AC0-441F-9BAD-1C9C2C35E306.png",
+                      "frame" : {
+                        "y" : 331,
+                        "x" : 2701,
+                        "width" : 294,
+                        "height" : 56
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 331,
+                      "x" : 2701,
+                      "width" : 294,
+                      "height" : 56
+                    },
+                    "name" : "Tweet"
+                  },
+                  {
+                    "maskFrame" : null,
+                    "id" : "952E5F86-8059-4504-B894-94361D6376E6",
+                    "visible" : true,
+                    "children" : [
+
+                    ],
+                    "image" : {
+                      "path" : "images\/Share-952E5F86-8059-4504-B894-94361D6376E6.png",
+                      "frame" : {
+                        "y" : 331,
+                        "x" : 2389,
+                        "width" : 293,
+                        "height" : 56
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 331,
+                      "x" : 2389,
+                      "width" : 293,
+                      "height" : 56
+                    },
+                    "name" : "Share"
+                  }
+                ],
+                "image" : {
+                  "path" : "images\/notification_exp_buttons-AACDA92C-7757-4A05-A13B-0741609FBAF4.png",
+                  "frame" : {
+                    "y" : 331,
+                    "x" : 2389,
+                    "width" : 606,
+                    "height" : 56
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 331,
+                  "x" : 2389,
+                  "width" : 606,
+                  "height" : 56
+                },
+                "name" : "notification_exp_buttons"
+              },
+              {
+                "maskFrame" : null,
+                "id" : "C6384938-7008-4B82-A0E8-12DFA52AE740",
+                "visible" : true,
+                "children" : [
+
+                ],
+                "image" : {
+                  "path" : "images\/notification_exp_line-C6384938-7008-4B82-A0E8-12DFA52AE740.png",
+                  "frame" : {
+                    "y" : 308,
+                    "x" : 2372,
+                    "width" : 640,
+                    "height" : 1
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 308,
+                  "x" : 2372,
+                  "width" : 640,
+                  "height" : 1
+                },
+                "name" : "notification_exp_line"
+              },
+              {
+                "maskFrame" : null,
+                "id" : "8CCE48BD-365D-4192-A3C0-B700A7D760C3",
+                "visible" : true,
+                "children" : [
+                  {
+                    "maskFrame" : {
+                      "y" : 59,
+                      "x" : 2302,
+                      "width" : 240,
+                      "height" : 240
+                    },
+                    "id" : "1715F5E4-4968-4845-A809-C497C37BCA60",
+                    "visible" : true,
+                    "children" : [
+                      {
+                        "maskFrame" : null,
+                        "id" : "BC555F7F-2E9C-409B-84D2-54CEABEDB645",
+                        "visible" : true,
+                        "children" : [
+
+                        ],
+                        "image" : {
+                          "path" : "images\/mini_icon_2-BC555F7F-2E9C-409B-84D2-54CEABEDB645.png",
+                          "frame" : {
+                            "y" : 156,
+                            "x" : 2396,
+                            "width" : 56,
+                            "height" : 49
+                          }
+                        },
+                        "imageType" : "png",
+                        "layerFrame" : {
+                          "y" : 156,
+                          "x" : 2396,
+                          "width" : 56,
+                          "height" : 49
+                        },
+                        "name" : "mini_icon_2"
+                      }
+                    ],
+                    "image" : {
+                      "path" : "images\/mini_icon-1715F5E4-4968-4845-A809-C497C37BCA60.png",
+                      "frame" : {
+                        "y" : 159,
+                        "x" : 2402,
+                        "width" : 40,
+                        "height" : 40
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 159,
+                      "x" : 2402,
+                      "width" : 40,
+                      "height" : 40
+                    },
+                    "name" : "mini_icon"
+                  }
+                ],
+                "image" : {
+                  "path" : "images\/notification_exp_content-8CCE48BD-365D-4192-A3C0-B700A7D760C3.png",
+                  "frame" : {
+                    "y" : 159,
+                    "x" : 2402,
+                    "width" : 559,
+                    "height" : 124
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 159,
+                  "x" : 2402,
+                  "width" : 559,
+                  "height" : 124
+                },
+                "name" : "notification_exp_content"
+              }
+            ],
+            "image" : {
+              "path" : "images\/notification_content_exp-112B2F1B-963A-43E7-BE01-EA372F4012EE.png",
+              "frame" : {
+                "y" : 159,
+                "x" : 2372,
+                "width" : 640,
+                "height" : 228
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 159,
+              "x" : 2372,
+              "width" : 640,
+              "height" : 228
+            },
+            "name" : "notification_content_exp"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "39CF4996-AEB8-4799-8C85-861CA5562BFE",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "A47B497B-A807-4ADF-A8BD-602CB70F91DA",
+                "visible" : true,
+                "children" : [
+
+                ],
+                "image" : {
+                  "path" : "images\/notification_bg_handle-A47B497B-A807-4ADF-A8BD-602CB70F91DA.png",
+                  "frame" : {
+                    "y" : 361,
+                    "x" : 1991,
+                    "width" : 74,
+                    "height" : 10
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 361,
+                  "x" : 1991,
+                  "width" : 74,
+                  "height" : 10
+                },
+                "name" : "notification_bg_handle"
+              },
+              {
+                "maskFrame" : {
+                  "y" : 122,
+                  "x" : 1638,
+                  "width" : 241,
+                  "height" : 240
+                },
+                "id" : "79131169-C83A-4A89-9691-B4BD1AD132A2",
+                "visible" : true,
+                "children" : [
+                  {
+                    "maskFrame" : null,
+                    "id" : "1B08BBBB-B94D-4F75-AC22-5C949429AAE6",
+                    "visible" : true,
+                    "children" : [
+
+                    ],
+                    "image" : {
+                      "path" : "images\/mini_icon_2-1B08BBBB-B94D-4F75-AC22-5C949429AAE6.png",
+                      "frame" : {
+                        "y" : 219,
+                        "x" : 1732,
+                        "width" : 56,
+                        "height" : 49
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 219,
+                      "x" : 1732,
+                      "width" : 56,
+                      "height" : 49
+                    },
+                    "name" : "mini_icon_2"
+                  }
+                ],
+                "image" : {
+                  "path" : "images\/mini_icon-79131169-C83A-4A89-9691-B4BD1AD132A2.png",
+                  "frame" : {
+                    "y" : 222,
+                    "x" : 1738,
+                    "width" : 40,
+                    "height" : 40
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 222,
+                  "x" : 1738,
+                  "width" : 40,
+                  "height" : 40
+                },
+                "name" : "mini_icon"
+              }
+            ],
+            "image" : {
+              "path" : "images\/notification_content-39CF4996-AEB8-4799-8C85-861CA5562BFE.png",
+              "frame" : {
+                "y" : 222,
+                "x" : 1738,
+                "width" : 559,
+                "height" : 149
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 222,
+              "x" : 1738,
+              "width" : 559,
+              "height" : 149
+            },
+            "name" : "notification_content"
+          }
+        ],
+        "image" : {
+          "path" : "images\/notification_all-50AB0289-DDE0-471E-ADDD-DAD3B553F1C7.png",
+          "frame" : {
+            "y" : 159,
+            "x" : 1738,
+            "width" : 1274,
+            "height" : 228
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 159,
+          "x" : 1738,
+          "width" : 1274,
+          "height" : 228
+        },
+        "name" : "notification_all"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "2087F9C1-FB25-425F-846C-6C9B4DAF6AE3",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/notification_bg-2087F9C1-FB25-425F-846C-6C9B4DAF6AE3.png",
+          "frame" : {
+            "y" : 47,
+            "x" : 1708,
+            "width" : 640,
+            "height" : 331
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 47,
+          "x" : 1708,
+          "width" : 640,
+          "height" : 331
+        },
+        "name" : "notification_bg"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "6D7D55D8-9812-4306-9248-38869F7737AF",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/screen_1-6D7D55D8-9812-4306-9248-38869F7737AF.png",
+          "frame" : {
+            "y" : 207,
+            "x" : 1708,
+            "width" : 640,
+            "height" : 1136
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 207,
+          "x" : 1708,
+          "width" : 640,
+          "height" : 1136
+        },
+        "name" : "screen_1"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "D05DD186-6A1D-4067-8BFE-08F5C67E4298",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "7CBC7A12-92E4-476B-97EE-A33733013FC5",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/notification_bg_exp-7CBC7A12-92E4-476B-97EE-A33733013FC5.png",
+              "frame" : {
+                "y" : 144,
+                "x" : 2372,
+                "width" : 640,
+                "height" : 265
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 144,
+              "x" : 2372,
+              "width" : 640,
+              "height" : 265
+            },
+            "name" : "notification_bg_exp"
+          }
+        ],
+        "image" : {
+          "path" : "images\/notification_exp-D05DD186-6A1D-4067-8BFE-08F5C67E4298.png",
+          "frame" : {
+            "y" : 144,
+            "x" : 2372,
+            "width" : 640,
+            "height" : 265
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 144,
+          "x" : 2372,
+          "width" : 640,
+          "height" : 265
+        },
+        "name" : "notification_exp"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "BBA13DB7-CB0C-410B-B96A-1BF948FEB047",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/screen_fake-BBA13DB7-CB0C-410B-B96A-1BF948FEB047.png",
+          "frame" : {
+            "y" : 47,
+            "x" : 1708,
+            "width" : 640,
+            "height" : 1138
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 47,
+          "x" : 1708,
+          "width" : 640,
+          "height" : 1138
+        },
+        "name" : "screen_fake"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "49D35EA8-1730-4461-BE5B-12DF5E4D266C",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/iphone-49D35EA8-1730-4461-BE5B-12DF5E4D266C.png",
+          "frame" : {
+            "y" : -186,
+            "x" : -310,
+            "width" : 776,
+            "height" : 1612
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : -186,
+          "x" : -310,
+          "width" : 776,
+          "height" : 1612
+        },
+        "name" : "iphone"
+      }
+    ],
+    "image" : {
+      "path" : "images\/shot-99FD0903-8F08-4475-B2D5-3AF620D1668D.png",
+      "frame" : {
+        "y" : 0,
+        "x" : 0,
+        "width" : 3597,
+        "height" : 2174
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 3597,
+      "height" : 2174
+    },
+    "name" : "shot"
+  }
+]
